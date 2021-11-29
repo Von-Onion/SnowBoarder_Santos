849f5907b17cc5b25d91ea64964861c54f624aad
Von-Onion <rumoto126@gmail.com>
2021-11-29 10:03:23 -0500

    Particles

--- SnowBoarder/Assets/Scripts/FinishLine.cs
+++ SnowBoarder/Assets/Scripts/FinishLine.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] float  loadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+        Invoke("ReloadScene", loadDelay);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
     }
 }
